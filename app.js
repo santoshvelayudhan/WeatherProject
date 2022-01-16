4734902ef2f748cd2cbdec2cb903839bd82f715d
Santosh Velayudhan <santoshvelayudhan@gmail.com>
2022-01-16 14:17:45 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express ();
+
+app.get ("/",function(req,res){
+
+res.send("server is up and running")
+
+})
+
+app.get ("/about",function(req,res){
+
+    res.send("santosh is gr8")
+    
+    })
+
+app.listen(3000,function()
+{
+
+    console.log("server is runnining in port 3000")
+})
